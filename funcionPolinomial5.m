a1f017f57af20ee9ff23655b79c082a80cedc4c8
AD1823AN <91568721+AD1823AN@users.noreply.github.com>
2021-11-21 15:58:29 -0600

    Add files via upload

---
+++ funcionPolinomial5.m
@@ -0,0 +1,17 @@
+% Octave Script
+% title               :FUNCIONES POLINOMILALES 
+% Description         :Script para graficar funciones polinomiales 
+% Author              :Adriana Trejo Patricio (Adri)
+% Date                :202123172 
+% Vercion             :1
+% Usage               :octave> /path/ClasificacionNumeros 
+% Notes               :Requiere aplicacion octave , usar su linea de comandos
+
+% limpiar variables
+clear 
+% Dominio de la funcion
+x = -10: 1: 20;
+% Funcion
+y = 3.^sqrt(x.^2);
+% Función Plotear 
+plot (x, y)
---
+++ funcionPolinomial6.m
@@ -0,0 +1,17 @@
+% Octave Script
+% title               :FUNCIONES POLINOMILALES 
+% Description         :Script para graficar funciones polinomiales 
+% Author              :Adriana Trejo Patricio (Adri)
+% Date                :202123172 
+% Vercion             :1
+% Usage               :octave> /path/ClasificacionNumeros 
+% Notes               :Requiere aplicacion octave , usar su linea de comandos
+
+% limpiar variables
+clear 
+% Dominio de la funcion
+x = -10: 1: 20;
+% Funcion
+y = x.^3-6*(x.^2)+11*x-6;
+% Función Plotear 
+plot (x, y)
